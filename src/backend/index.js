32f8b6f08b421a24cab8ca62d64309edc67d3a9d
dylan <gbujieshi@gmail.com>
2017-08-25 14:49:26 +0800

    automate server startup

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,38 +1,11 @@
 'use strict';
 
-var Percolator = require('percolator').Percolator;
-var dbSession = require('./dbSession.js');
+var Server = require('./server.js').Server;
 
-var port = 8080;
-var server = Percolator({'port': port, 'autoLink': false});
-
-server.route('/api/keywords',
-	{
-		GET: function(req, res){
-			dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id',
-                function(err, rows){
-                    if(err){
-                        console.log(err);
-                        res.status.internalServerError(err);
-                    }else{
-                        res.collection(rows).send(); 
-                    }
-                })
-		}
-	}
-);
+var server = Server('8080');
 
 server.listen(function(){
 	console.log('Server started and listening on port', port);
 });
 
-/*const express = require('express')
-const app = express()
 
-app.get('/api/keywords', function (req, res) {
-  res.send({'foo':'bar'});
-})
-
-app.listen(8080, function () {
-  console.log('Server started and listening on port 8080!')
-})*/
